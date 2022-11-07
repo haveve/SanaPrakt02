43a275745326d5532d4f8b1fb490af1dba090f48
IvanPogoidash <ipz224_pis@student.ztu.edu.ua>
2022-11-07 13:26:23 +0200

    line from 2 to 4 has been updated ('C++ is better ' has been replaced by 'C++ is better in the world ')

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("C++ is better ");
-Console.WriteLine("C++ is better ");
+Console.WriteLine("C++ is better in the world ");
+Console.WriteLine("C++ is better in the world ");
 Console.WriteLine("C++ is better ");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
